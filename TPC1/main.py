5e6f88945e1d9410d5ea311b48800bf4334ab470
Samba@X13 <santos.diogo.21@outlook.com>
2024-02-15 13:25:29 +0000

    Completo sem o md

--- TPC1/main.py
+++ TPC1/main.py
@@ -6,6 +6,12 @@ import sys
         Percentagens de atletas aptos e inaptos para a prática desportiva;
         Distribuição de atletas por escalão etário (escalão = intervalo de 5 anos): ... [30-34], [35-39], ... """
 
+def setEscalao (escaloes, idade):
+    range= idade- (idade % 5)
+    if range not in escaloes:
+        escaloes[range]= 0
+    escaloes[range]+= 1
+
 def main ():
     modalidades= set()
     escaloes= {}
@@ -15,25 +21,28 @@ def main ():
     header= sys.stdin.readline()
     for line in sys.stdin:
         tokens= line.split(",")
+        todos+= 1   
 
         i= 0
         for token in tokens:
             if (i== 5):
-                idade = token
+                setEscalao (escaloes, int(token)) 
             elif (i== 8):
                 modalidades.add(token)
             elif (i== 12):
-                if (token):
+                if (token== 'true'):
+                    print (token)
                     aptos+= 1
+            i+=1
 
-            todos+= 1
-            i+= 1
-
-    print (modalidades.sort())
-    percentagem= aptos/ todos* 100
-    print (percentagem)
+    modalidadesL= list(modalidades)
+    modalidadesL.sort()
+    print ("\nModalidades")
+    print (modalidadesL)
+    print ("\nPorcentagem de atletas aptos:")
+    porcentagem= aptos/ todos* 100
+    print (porcentagem)
+    print ("\nDistribuicao por escaloes:")
     print (escaloes)
 
-""" Escaloes ainda nao esta feito """
-        
 main()
